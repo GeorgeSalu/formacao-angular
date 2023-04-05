97f2b382b6b7bf191805fd3e3b15579c42118857
GeorgeSalu <george.salu10@gmail.com>
2023-04-05 11:38:30 -0300

    projeto gerenciador de tarefas: implementando a edicao de tarefas

--- gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.ts
+++ gerenciador-de-tarefas/src/app/tarefas/editar/editar-tarefa.component.ts
@@ -10,7 +10,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EditarTarefaComponent implements OnInit {
 
-  @ViewChild('formTarefa') formTarefa: NgForm;
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
   tarefa: Tarefa;
 
   constructor(private tarefaService: TarefaService,
