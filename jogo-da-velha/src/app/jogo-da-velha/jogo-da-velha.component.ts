f0ff39286d99db8a8aca126d1a8fac96dd9b95c2
GeorgeSalu <george.salu10@gmail.com>
2023-04-06 14:20:31 -0300

    projeto jogo da velha: implementando a tela inical do jogo da velha

--- jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
+++ jogo-da-velha/src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { JogoDaVelhaService } from './shared';
+
 @Component({
   selector: 'app-jogo-da-velha',
   templateUrl: './jogo-da-velha.component.html',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoDaVelhaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  	this.jogoDaVelhaService.inicializar();
   }
+
+  get showInicio(): boolean {
+  	return this.jogoDaVelhaService.showInicio;
+  }
+
+  /**
+   * Retorna se o tabuleiro deve ser exibido.
+   *
+   * @return boolean
+   */
+  get showTabuleiro(): boolean {
+  	return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+  /**
+   * Retorna se a tela de fim de jogo deve ser exibida.
+   *
+   * @return boolean
+   */
+  get showFinal(): boolean {
+  	return this.jogoDaVelhaService.showFinal;
+  }
+
+  iniciarJogo(): void {
+  	this.jogoDaVelhaService.iniciarJogo();
+  }
+
 
 }
