d7d6257bb803d7cb1432898cd554303d8232b4d1
GeorgeSalu <george.salu10@gmail.com>
2023-04-09 13:59:00 -0300

    projeto conversor de moedas: corrigindo imports

--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConversorComponent } from './components/conversor.component';
+import { ConversorComponent } from './components';
 
 @NgModule({
   declarations: [
