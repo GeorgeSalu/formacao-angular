0c384feec6162b272d3a1008c7c3db18247fe770
GeorgeSalu <george.salu10@gmail.com>
2023-04-11 13:49:32 -0300

    projeto conversor de moedas: criando uma directiva de validacao de numeros

--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -4,10 +4,12 @@ import { ConversorComponent } from './components';
 import { ConversorService, MoedaService } from './services';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NumeroDirective } from './directives/numero.directive';
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
     CommonModule,
