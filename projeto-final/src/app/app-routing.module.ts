b9490f10c6f5295cdba8862e533c5bb47128f8bb
GeorgeSalu <george.salu10@gmail.com>
2023-04-17 19:00:17 -0300

    projeto agregador: criando as rotas do modulo gerenciador de tarefas

--- projeto-final/src/app/app-routing.module.ts
+++ projeto-final/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardRoutes } from './dashboard/dashboard-routing.module';
 import { CalculadoraRoutes } from './calculadora/calculadora-routing.module';
 import { ConversorRoutes } from './conversor/conversor-routing.module';
+import { TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [
   {
@@ -12,7 +13,8 @@ export const routes: Routes = [
   },
   ...DashboardRoutes,
   ...CalculadoraRoutes,
-  ...ConversorRoutes
+  ...ConversorRoutes,
+  ...TarefaRoutes
 ];
 
 @NgModule({
