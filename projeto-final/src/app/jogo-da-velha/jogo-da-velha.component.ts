380bb9b7710a83d75300825594f4227cb179a1dc
GeorgeSalu <george.salu10@gmail.com>
2023-04-20 09:57:43 -0300

    projeto agregador: integrando o modulo jogo da velha

---
+++ projeto-final/src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+
+import { JogoDaVelhaService } from './shared';
+
+@Component({
+  selector: 'app-jogo-da-velha',
+  templateUrl: './jogo-da-velha.component.html',
+  styleUrls: ['./jogo-da-velha.component.css']
+})
+export class JogoDaVelhaComponent implements OnInit {
+
+  constructor(private jogoDaVelhaService: JogoDaVelhaService) { }
+
+  ngOnInit() {
+  	this.jogoDaVelhaService.inicializar();
+  }
+
+  get showInicio(): boolean {
+  	return this.jogoDaVelhaService.showInicio;
+  }
+
+  /**
+   * Retorna se o tabuleiro deve ser exibido.
+   *
+   * @return boolean
+   */
+  get showTabuleiro(): boolean {
+  	return this.jogoDaVelhaService.showTabuleiro;
+  }
+
+  /**
+   * Retorna se a tela de fim de jogo deve ser exibida.
+   *
+   * @return boolean
+   */
+  get showFinal(): boolean {
+  	return this.jogoDaVelhaService.showFinal;
+  }
+
+  iniciarJogo(): void {
+  	this.jogoDaVelhaService.iniciarJogo();
+  }
+
+  jogar(posX: number, posY: number): void {
+  	this.jogoDaVelhaService.jogar(posX, posY);
+  }
+
+  /**
+   * Retorna se a peça X deve ser exibida para a
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirX(posX: number, posY: number): boolean {
+  	return this.jogoDaVelhaService.exibirX(posX, posY);
+  }
+
+  /**
+   * Retorna se a peça O deve ser exibida para a
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirO(posX: number, posY: number): boolean {
+  	return this.jogoDaVelhaService.exibirO(posX, posY);
+  }
+
+  /**
+   * Retorna se a marcação de vitória deve ser exibida para a
+   * coordena informada.
+   *
+   * @param number posX
+   * @param number posY
+   * @return boolean
+   */
+  exibirVitoria(posX: number, posY: number): boolean {
+  	return this.jogoDaVelhaService.exibirVitoria(posX, posY);
+  }
+
+  get jogador(): number {
+  	return this.jogoDaVelhaService.jogador;
+  }
+
+  /**
+   * Inicia um novo jogo.
+   *
+   * @return void
+   */
+  novoJogo(): void {
+  	this.jogoDaVelhaService.novoJogo();
+  }
+}
